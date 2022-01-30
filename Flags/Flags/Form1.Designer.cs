3e100d9e0bfbf69e32ba2d18166fc20e62dd1da1
Categio <categio1@gmail.com>
2022-01-30 08:25:23 -0800

    Added a flag identifying program.

---
+++ Flags/Flags/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace Flags
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.countryLabel = new System.Windows.Forms.Label();
+            this.finlandPictureBox = new System.Windows.Forms.PictureBox();
+            this.francePictureBox = new System.Windows.Forms.PictureBox();
+            this.germanyPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.finlandPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.francePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.germanyPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(195, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Click a flag to see the name of the country.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // countryLabel
+            // 
+            this.countryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countryLabel.Location = new System.Drawing.Point(199, 279);
+            this.countryLabel.Name = "countryLabel";
+            this.countryLabel.Size = new System.Drawing.Size(305, 38);
+            this.countryLabel.TabIndex = 1;
+            this.countryLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // finlandPictureBox
+            // 
+            this.finlandPictureBox.ErrorImage = ((System.Drawing.Image)(resources.GetObject("finlandPictureBox.ErrorImage")));
+            this.finlandPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("finlandPictureBox.Image")));
+            this.finlandPictureBox.Location = new System.Drawing.Point(41, 134);
+            this.finlandPictureBox.Name = "finlandPictureBox";
+            this.finlandPictureBox.Size = new System.Drawing.Size(152, 90);
+            this.finlandPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.finlandPictureBox.TabIndex = 2;
+            this.finlandPictureBox.TabStop = false;
+            this.finlandPictureBox.Click += new System.EventHandler(this.finlandPictureBox_Click);
+            // 
+            // francePictureBox
+            // 
+            this.francePictureBox.ErrorImage = ((System.Drawing.Image)(resources.GetObject("francePictureBox.ErrorImage")));
+            this.francePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("francePictureBox.Image")));
+            this.francePictureBox.Location = new System.Drawing.Point(258, 134);
+            this.francePictureBox.Name = "francePictureBox";
+            this.francePictureBox.Size = new System.Drawing.Size(155, 90);
+            this.francePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.francePictureBox.TabIndex = 3;
+            this.francePictureBox.TabStop = false;
+            this.francePictureBox.Click += new System.EventHandler(this.francePictureBox_Click);
+            // 
+            // germanyPictureBox
+            // 
+            this.germanyPictureBox.ErrorImage = ((System.Drawing.Image)(resources.GetObject("germanyPictureBox.ErrorImage")));
+            this.germanyPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("germanyPictureBox.Image")));
+            this.germanyPictureBox.Location = new System.Drawing.Point(480, 134);
+            this.germanyPictureBox.Name = "germanyPictureBox";
+            this.germanyPictureBox.Size = new System.Drawing.Size(151, 90);
+            this.germanyPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.germanyPictureBox.TabIndex = 4;
+            this.germanyPictureBox.TabStop = false;
+            this.germanyPictureBox.Click += new System.EventHandler(this.germanyPictureBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 350);
+            this.Controls.Add(this.germanyPictureBox);
+            this.Controls.Add(this.francePictureBox);
+            this.Controls.Add(this.finlandPictureBox);
+            this.Controls.Add(this.countryLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Identify Flags";
+            ((System.ComponentModel.ISupportInitialize)(this.finlandPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.francePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.germanyPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label countryLabel;
+        private System.Windows.Forms.PictureBox finlandPictureBox;
+        private System.Windows.Forms.PictureBox francePictureBox;
+        private System.Windows.Forms.PictureBox germanyPictureBox;
+    }
+}
+
---
+++ Flags/Flags/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flags
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void finlandPictureBox_Click(object sender, EventArgs e)
+        {
+            countryLabel.Text = "This is Finland's Flag!";
+        }
+
+        private void francePictureBox_Click(object sender, EventArgs e)
+        {
+            countryLabel.Text = "This is France's Flag!";
+        }
+
+        private void germanyPictureBox_Click(object sender, EventArgs e)
+        {
+            countryLabel.Text = "This is Germany's Flag!";
+        }
+    }
+}
