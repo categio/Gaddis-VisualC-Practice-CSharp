f2849a6f5721578971b29ce3617fc802f899010b
Categio <categio1@gmail.com>
2022-02-06 23:01:25 -0800

    finished fuel economy calculator, and added a clear data button so that the user may clear and enter new data to calculate fuel economy data for.

--- Fuel Economy/Fuel Economy/Form1.Designer.cs
+++ Fuel Economy/Fuel Economy/Form1.Designer.cs
@@ -37,6 +37,7 @@ namespace Fuel_Economy
             this.mpgLabel = new System.Windows.Forms.Label();
             this.calculateButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // milesPromptLabel
@@ -105,21 +106,33 @@ namespace Fuel_Economy
             // 
             // calculateButton
             // 
-            this.calculateButton.Location = new System.Drawing.Point(73, 178);
+            this.calculateButton.Location = new System.Drawing.Point(14, 178);
             this.calculateButton.Name = "calculateButton";
             this.calculateButton.Size = new System.Drawing.Size(92, 49);
             this.calculateButton.TabIndex = 8;
             this.calculateButton.Text = "Calculate MPG";
             this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
             // 
             // exitButton
             // 
-            this.exitButton.Location = new System.Drawing.Point(187, 178);
+            this.exitButton.Location = new System.Drawing.Point(230, 178);
             this.exitButton.Name = "exitButton";
             this.exitButton.Size = new System.Drawing.Size(92, 49);
             this.exitButton.TabIndex = 9;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(122, 178);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(92, 49);
+            this.clearButton.TabIndex = 10;
+            this.clearButton.Text = "Clear Data";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
             // 
             // Form1
             // 
@@ -129,6 +142,7 @@ namespace Fuel_Economy
             this.BackColor = System.Drawing.Color.SeaShell;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(346, 249);
+            this.Controls.Add(this.clearButton);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.calculateButton);
             this.Controls.Add(this.mpgLabel);
@@ -157,6 +171,7 @@ namespace Fuel_Economy
         private System.Windows.Forms.Label mpgLabel;
         private System.Windows.Forms.Button calculateButton;
         private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button clearButton;
     }
 }
 
